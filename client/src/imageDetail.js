171b1fc7c8473cf405f0d8ad6923cdd35467fb00
im-abhinav <meetspacegravity@gmail.com>
2020-09-26 09:39:23 +0530

    newdata

--- client/src/imageDetail.js
+++ client/src/imageDetail.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
-//import ReactMapboxGl, { Layer, Feature, Marker, Popup } from "react-mapbox-gl";
-import { Map, Popup, TileLayer, Marker } from "react-leaflet";
+import ReactMapboxGl, { Popup } from "react-mapbox-gl";
 
 import axios from "axios";
 import "./imageDetail.css";
 
-// const Map = ReactMapboxGl({
-//   accessToken:
-//     "pk.eyJ1Ijoid2l6YXJkY29kaW5nIiwiYSI6ImNrYXh6ejVzMDBiN3UyeG5pZHV3ZDl1eGUifQ.3yx5c45P8A1cmOSyycXugQ",
-// });
+const Map = ReactMapboxGl({
+  accessToken:
+    "pk.eyJ1Ijoid2l6YXJkY29kaW5nIiwiYSI6ImNrYXh6ejVzMDBiN3UyeG5pZHV3ZDl1eGUifQ.3yx5c45P8A1cmOSyycXugQ",
+});
 
 const DEFAULT_LANGITUDE = -123;
 const DEFAULT_LATITUDE = 48;
@@ -40,17 +39,28 @@ class ImageDetails extends Component {
     return (
       <div>
         {this.state.loading ? (
-          <Map center={[latitude, langitude]} zoom={13}>
-            <TileLayer
-              url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            ></TileLayer>
-            {!this.state.loading ? (
-              <div className="loading">loading</div>
+          <Map
+            style="mapbox://styles/mapbox/streets-v9"
+            containerStyle={{
+              height: "100vh",
+              width: "100%",
+            }}
+            center={[latitude, langitude]}
+            zoom={[1]}
+          >
+            {this.state.loading ? (
+              <Popup
+                coordinates={[latitude, langitude]}
+                offset={{
+                  "bottom-left": [12, -38],
+                  bottom: [0, -38],
+                  "bottom-right": [-12, -38],
+                }}
+              >
+                <img src={this.state.data.image} height="100px" width="100px" />
+              </Popup>
             ) : (
-              <Marker position={[latitude, langitude]}>
-                <Popup>You are here!</Popup>
-              </Marker>
+              <p>Null</p>
             )}
           </Map>
         ) : (
@@ -62,30 +72,3 @@ class ImageDetails extends Component {
 }
 
 export default ImageDetails;
-
-{
-  /* <Map
-            style="mapbox://styles/mapbox/streets-v9"
-            containerStyle={{
-              height: "100vh",
-              width: "100%",
-            }}
-            center={[this.state.data.lat, this.state.data.long]}
-            zoom={[1]}
-          >
-            {this.state.loading ? (
-              <Popup
-                coordinates={[this.state.data.lat, this.state.data.long]}
-                offset={{
-                  "bottom-left": [12, -38],
-                  bottom: [0, -38],
-                  "bottom-right": [-12, -38],
-                }}
-              >
-                <img src={this.state.data.image} height="100px" width="100px" />
-              </Popup>
-            ) : (
-              <p>Null</p>
-            )} */
-}
-// </Map>
