b7cc05bbcf2dc50f39efacbeb9b1242ddcd1965c
im-abhinav <meetspacegravity@gmail.com>
2020-09-26 11:24:11 +0530

    finishv1 h

--- client/src/file-upload.js
+++ client/src/file-upload.js
@@ -57,7 +57,7 @@ class FileUpload extends Component {
           <div className="container">
             <div className="col-md-12">
               <form onSubmit={this.onSubmit}>
-                <h4 className="text-center mt-4 mb-5 text">Upload An Image</h4>
+                <h4 className="text-center mt-6 mb-5 text">Upload An Image</h4>
                 <div className="form-group">
                   <input
                     type="text"
